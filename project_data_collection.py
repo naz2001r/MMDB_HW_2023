75f36a2d5b6678380f9a837e250e2963cd1d1526
ibabin <igor.babin185@gmail.com>
2023-11-10 13:34:37 +0200

    Change data saving

--- project_data_collection.py
+++ project_data_collection.py
@@ -1,11 +1,24 @@
 import json
-from sseclient import SSEClient as EventSource
+import logging
+import os
 
+from sseclient import SSEClient as EventSource
+import datetime
+# remove buffered output
+os.environ["PYTHONUNBUFFERED"] = "1"
 EVENTS = 10e6  # 1 million
-SAVE_FREQ = 5000
+SAVE_FREQ = 50000
 URL = 'https://stream.wikimedia.org/v2/stream/recentchange'
 
+logger = logging.getLogger("")
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
 if __name__ == '__main__':
+    # print('Starting data collection')
+    logger.addHandler(ch)
+    logger.info('Starting data collection')
     # Collect data from the stream as csv file
     count = 0
     data = []
@@ -25,9 +38,10 @@ if __name__ == '__main__':
                 break
 
             if count % SAVE_FREQ == 0:
-                with open('data.json', 'w') as outfile:
+                with open(f'data_{count}.json', 'w') as outfile:
                     json.dump(data, outfile)
-                print('Saved {} events'.format(count))
+                    data = []
+                logger.info('Time: {}, saved {} events'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), count))
 
             # if count % 50 == 0:
             #     print('Processed {} events'.format(count))
--- project_model_fit.py
+++ project_model_fit.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import json
 
 class RandomClassificator:
     def __init__(self):
@@ -14,5 +14,8 @@ class RandomClassificator:
 
 if __name__ == '__main__':
     # TODO FIT MODEL AND SAVE RESULTS
-    for _ in range(10):
-        print(RandomClassificator().predict())
+    # for _ in range(10):
+    #     print(RandomClassificator().predict())
+    with open('data1.json', 'r') as f:
+        data = json.load(f)
+    print(len(data))
